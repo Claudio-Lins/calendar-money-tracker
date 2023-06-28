501e1c4185de58b025b70a743621c3c1053e8257
Claudio Lins <claudio.lins@me.com>
2023-06-28 22:23:55 +0100

    Update ContentCell.tsx

--- src/components/cell/ContentCell.tsx
+++ src/components/cell/ContentCell.tsx
@@ -11,15 +11,32 @@ export function ContentCell({ entries }: ContentCellProps) {
       <tbody className="">
         {entries?.map((entry) => (
           <tr key={entry.id} className="border-b-[0.5px] border-zinc-500">
-            <td className="w-full py-2 whitespace-nowrap">{entry.title}</td>
-            <td className="w-full py-2 whitespace-nowrap">
+            <td
+              className={`
+              w-full py-2 whitespace-nowrap
+              ${entry.type === "INCOME" ? "text-green-300" : "text-red-300"}
+              `}
+            >
+              {entry.title}
+            </td>
+            <td
+              className={`
+              w-full py-2 whitespace-nowrap
+              ${entry.type === "INCOME" ? "text-green-300" : "text-red-300"}
+              `}
+            >
               {new Intl.NumberFormat("pt-PT", {
                 style: "currency",
                 currency: "EUR",
               }).format(entry.amount / 100)}
             </td>
             <td className="w-full py-2 whitespace-nowrap">
-              <Eye size={12} />
+              <Eye
+                size={12}
+                className={` ${
+                  entry.type === "INCOME" ? "text-green-300" : "text-red-300"
+                }`}
+              />
             </td>
           </tr>
         ))}

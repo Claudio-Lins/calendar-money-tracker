e447b984b54289f1e02bb180cb13ab37814e7033
Claudio Lins <claudio.lins@me.com>
2023-06-29 13:59:53 +0100

    Update route.tsx

--- src/app/api/entries/route.tsx
+++ src/app/api/entries/route.tsx
@@ -2,38 +2,9 @@ import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
-  const body = await request.json();
-  const entrie = await prisma.entrie.create({
-    data: {
-      title: body.title,
-      amount: body.amount,
-      type: body.type,
-    },
-  });
-  return NextResponse.json({
-    entrie,
-  });
-}
-
-export async function PUT(request: NextRequest) {
-  const body = await request.json();
-  const entrie = await prisma.entrie.update({
-    where: { id: body.id },
-    data: {
-      title: body.title,
-      amount: body.amount,
-      type: body.type,
-    },
-  });
-  return NextResponse.json({
-    entrie,
-  });
-}
-
 export async function DELETE(request: NextRequest) {
   const body = await request.json();
-  const entrie = await prisma.entrie.delete({
+  const entrie = await prisma.entry.delete({
     where: { id: body.id },
   });
   return NextResponse.json({
@@ -42,7 +13,7 @@ export async function DELETE(request: NextRequest) {
 }
 
 export async function GET(request: NextRequest) {
-  const entries = await prisma.entrie.findMany();
+  const entries = await prisma.entry.findMany();
   return NextResponse.json({
     entries,
   });

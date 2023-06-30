2eced74d9dd3d000084b3b7e331f5aeb595237e2
Claudio Lins <claudio.lins@me.com>
2023-06-30 12:30:10 +0100

    Cell component

--- src/components/cell/Cell.tsx
+++ src/components/cell/Cell.tsx
@@ -3,17 +3,70 @@ import { HeaderCell } from "./HeaderCell";
 import { FooterCell } from "./FooterCell";
 import { ContentCell } from "./ContentCell";
 import { EntryTypes } from "@/@types/EntryTypes";
+import { Eye } from "lucide-react";
 
 const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 export function Cell({ entries }: EntryTypes) {
   return (
-    <div className="w-[calc(100%/5)] h-[calc(100vw/5)] rounded-xl border-[0.5px] border-zinc-600 text-zinc-300 bg-zinc-700 overflow-y-auto flex flex-col items-center justify-between shadow-sm">
-      <HeaderCell day={20} daysWeek="Thu" />
-      <div className="w-full p-2 h-full">
-        <ContentCell entries={entries} />
-      </div>
-      <FooterCell />
-    </div>
+    <>
+      {entries
+        .filter((entry) => {
+          return entry.details.length > 0;
+        })
+        .map((entry) => (
+          <div
+            key={entry.id}
+            className="w-[calc(100%/5)] h-[calc(100vw/5)] rounded-xl border-[0.5px] border-zinc-600 text-zinc-300 bg-zinc-700 overflow-y-auto flex flex-col items-center justify-between shadow-sm"
+          >
+            <HeaderCell
+              day={new Intl.DateTimeFormat("pt-PT", {
+                day: "2-digit",
+              }).format(new Date(entry.updatedAt))}
+              daysWeek={new Intl.DateTimeFormat("us", {
+                weekday: "short",
+              })
+                .format(new Date(entry.updatedAt))
+                .slice(0, 3)}
+            />
+            <div className="w-full p-2 h-full">
+              <div className="min-w-full text-zinc-300 text-xs">
+                <div
+                  className={`flex flex-col gap-2
+            
+            `}
+                >
+                  {entry.details.map((details) => (
+                    <div
+                      key={details.id}
+                      className={`flex items-center justify-between w-full border-b border-zinc-600 pb-1
+                    ${
+                      details.type === "INCOME"
+                        ? "text-green-300"
+                        : "text-red-300"
+                    }
+                    `}
+                    >
+                      <p>{details.description}</p>
+                      <div className="flex items-center gap-1">
+                        <strong>
+                          {new Intl.NumberFormat("pt-PT", {
+                            style: "currency",
+                            currency: "EUR",
+                          }).format(details.amount)}
+                        </strong>
+                        <button className="w-full py-2 whitespace-nowrap">
+                          <Eye size={12} />
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <FooterCell />
+          </div>
+        ))}
+    </>
   );
 }

41fb820db50c4a46076308c91941f4efc4a19e91
Claudio Lins <claudio.lins@me.com>
2023-07-04 18:16:21 +0100

    totalIncomeAmount + Vercel Postgres

--- src/components/cell/Cell.tsx
+++ src/components/cell/Cell.tsx
@@ -3,8 +3,12 @@ import React, { useEffect, useState } from "react";
 import { HeaderCell } from "./HeaderCell";
 import { EntryTypes } from "@/@types/EntryTypes";
 import { Eye } from "lucide-react";
+import { useNewEntrie } from "@/context/entriesStore";
 
 export function Cell({ entries }: EntryTypes) {
+  const { setTotalExpense, setTotalIncome, totalExpense, totalIncome } =
+    useNewEntrie();
+
   return (
     <>
       {entries
@@ -18,79 +22,82 @@ export function Cell({ entries }: EntryTypes) {
             new Date(a.entryDetails[0].createdAt).getTime()
           );
         })
-        .map((entry) => (
-          <div
-            key={entry.id}
-            className="relative overflow-hidden w-[calc(100%/5)] h-[calc(100vw/5)] rounded-xl border-[0.5px] border-zinc-600 text-zinc-300 bg-zinc-700  flex flex-col items-center justify-between shadow-sm"
-          >
-            <HeaderCell
-              day={new Intl.DateTimeFormat("pt-PT", {
-                day: "2-digit",
-              }).format(new Date(entry.entryDetails[0].createdAt))}
-              daysWeek={new Intl.DateTimeFormat("us", {
-                weekday: "short",
-              })
-                .format(new Date(entry.entryDetails[0].createdAt))
-                .slice(0, 3)}
-            />
-            <div className="w-full h-full overflow-y-scroll p-2 ">
-              <div className="min-w-full text-zinc-300 text-xs">
-                <div
-                  className={`flex flex-col gap-2
+        .map((entry) => {
+          const totalExpenseAmount = entry.entryDetails
+            .filter((details) => details.type === "EXPENSE")
+            .reduce((acc, curr) => acc + curr.amount, 0);
+          const totalIncomeAmount = entry.entryDetails
+            .filter((details) => details.type === "INCOME")
+            .reduce((acc, curr) => acc + curr.amount, 0);
+          return (
+            <div
+              key={entry.id}
+              className="relative overflow-hidden w-[calc(100%/5)] h-[calc(100vw/5)] rounded-xl border-[0.5px] border-zinc-600 text-zinc-300 bg-zinc-700  flex flex-col items-center justify-between shadow-sm"
+            >
+              <HeaderCell
+                day={new Intl.DateTimeFormat("pt-PT", {
+                  day: "2-digit",
+                }).format(new Date(entry.entryDetails[0].createdAt))}
+                daysWeek={new Intl.DateTimeFormat("us", {
+                  weekday: "short",
+                })
+                  .format(new Date(entry.entryDetails[0].createdAt))
+                  .slice(0, 3)}
+              />
+              <div className="w-full h-full overflow-y-scroll p-2 ">
+                <div className="min-w-full text-zinc-300 text-xs">
+                  <div
+                    className={`flex flex-col gap-2
             
             `}
-                >
-                  {entry.entryDetails.map((details) => (
-                    <div
-                      key={details.id}
-                      className={`flex items-center justify-between w-full border-b border-zinc-600 pb-1
+                  >
+                    {entry.entryDetails.map((details) => (
+                      <div
+                        key={details.id}
+                        className={`flex items-center justify-between w-full border-b border-zinc-600 pb-1
               ${details.type === "INCOME" ? "text-green-300" : "text-red-300"}
               `}
-                    >
-                      <p>{details.description}</p>
-                      <div className="flex items-center gap-1">
-                        <strong>
-                          {new Intl.NumberFormat("pt-PT", {
-                            style: "currency",
-                            currency: "EUR",
-                          }).format(details.amount)}
-                        </strong>
-                        <button className="w-full py-2 whitespace-nowrap">
-                          <Eye size={12} />
-                        </button>
+                      >
+                        <p>{details.description}</p>
+                        <div className="flex items-center gap-1">
+                          <strong>
+                            {new Intl.NumberFormat("pt-PT", {
+                              style: "currency",
+                              currency: "EUR",
+                            }).format(details.amount)}
+                          </strong>
+                          <button className="w-full py-2 whitespace-nowrap">
+                            <Eye size={12} />
+                          </button>
+                        </div>
                       </div>
-                    </div>
-                  ))}
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="bottom-0  w-full flex items-center justify-between bg-zinc-800 px-2 h-12">
+                <div className="text-sm text-red-600 font-semibold">
+                  {new Intl.NumberFormat("pt", {
+                    style: "currency",
+                    currency: "EUR",
+                  }).format(totalExpenseAmount)}
+                </div>
+                <div className="text-lg text-white font-bold">
+                  {new Intl.NumberFormat("pt", {
+                    style: "currency",
+                    currency: "EUR",
+                  }).format(totalIncomeAmount - totalExpenseAmount)}
+                </div>
+                <div className="text-sm text-blue-600 font-semibold">
+                  {new Intl.NumberFormat("pt", {
+                    style: "currency",
+                    currency: "EUR",
+                  }).format(totalIncomeAmount)}
                 </div>
               </div>
             </div>
-            <div className="bottom-0  w-full flex items-center justify-between bg-zinc-800 px-2 h-12">
-              <div className="text-sm text-red-600 font-semibold">
-                {new Intl.NumberFormat("pt", {
-                  style: "currency",
-                  currency: "EUR",
-                }).format(
-                  entry.entryDetails
-                    .filter((details) => details.type === "EXPENSE")
-                    .reduce((acc, curr) => acc + curr.amount, 0)
-                )}
-              </div>
-              {/* <div className="text-lg text-white font-bold">
-                {entry.details.reduce((acc, curr) => acc + curr.amount, 0)}
-              </div> */}
-              <div className="text-sm text-blue-600 font-semibold">
-                {new Intl.NumberFormat("pt", {
-                  style: "currency",
-                  currency: "EUR",
-                }).format(
-                  entry.entryDetails
-                    .filter((details) => details.type === "INCOME")
-                    .reduce((acc, curr) => acc + curr.amount, 0)
-                )}
-              </div>
-            </div>
-          </div>
-        ))}
+          );
+        })}
     </>
   );
 }
--- src/context/entriesStore.ts
+++ src/context/entriesStore.ts
@@ -5,6 +5,10 @@ type NewEntryState = {
   setIsOpen: (isOpen: boolean) => void;
   typeOfEntry: string;
   setTypeOfEntry: (typeOfEntry: string) => void;
+  totalExpense: number;
+  setTotalExpense: (totalExpense: number) => void;
+  totalIncome: number;
+  setTotalIncome: (totalIncome: number) => void;
 };
 
 const initialState: NewEntryState = {
@@ -12,10 +16,16 @@ const initialState: NewEntryState = {
   setTypeOfEntry: () => {},
   isOpen: false,
   setIsOpen: () => {},
+  totalExpense: 0,
+  setTotalExpense: () => {},
+  totalIncome: 0,
+  setTotalIncome: () => {},
 };
 
 export const useNewEntrie = create<NewEntryState>((set) => ({
   ...initialState,
   setIsOpen: (isOpen) => set({ isOpen }),
   setTypeOfEntry: (typeOfEntry) => set({ typeOfEntry }),
+  setTotalExpense: (totalExpense) => set({ totalExpense }),
+  setTotalIncome: (totalIncome) => set({ totalIncome }),
 }));

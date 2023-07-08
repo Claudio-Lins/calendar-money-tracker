52914abf6b850e662f17f7109512cc6e61780661
Claudio Lins <claudio.lins@me.com>
2023-07-09 00:11:39 +0100

    Reac-hook-form and zod ok

--- src/components/cell/Form.tsx
+++ src/components/cell/Form.tsx
@@ -4,29 +4,42 @@ import { useNewEntrie } from "@/context/entriesStore";
 import { useModal } from "@/context/modalStore";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "react-hot-toast";
+
+const createEntryFormSchema = z.object({
+  description: z.string().nonempty("A descrição é obrigátoria"),
+  amount: z.number().optional(),
+  type: z.string().optional(),
+  locale: z.string().optional(),
+  createdAt: z.date().optional(),
+});
 
 interface FormDataProps {
   description?: string;
   amount: number | string;
   type: string;
   locale?: string;
-  createdAt?: Date;
+  createdAt?: Date | string;
 }
 export function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(createEntryFormSchema),
+  });
   const router = useRouter();
   const { typeOfEntry } = useNewEntrie();
   const {
     setIsOpen,
-    isSlideLeftOpen,
-    isSlideRightOpen,
     setIsSlideLeftOpen,
     setIsSlideRightOpen,
     setFormExpense,
     setFormIncome,
-    esquerda,
-    setEsquerda,
-    direita,
-    setDireita,
   } = useModal();
 
   const [formData, setFormData] = useState<FormDataProps>({
@@ -36,16 +49,6 @@ export function Form() {
     locale: "",
     createdAt: new Date(),
   });
-
-  function resetForm() {
-    setFormData({
-      description: "",
-      amount: "",
-      type: typeOfEntry,
-      locale: "",
-      createdAt: new Date(),
-    });
-  }
 
   function resetSlide() {
     setIsSlideLeftOpen(false);
@@ -87,71 +90,54 @@ export function Form() {
     }
   }
 
-  async function handleCreateEntry(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
+  async function handleCreateEntry(formData: any) {
     criarEntry(formData);
+    toast.success("Entrada criada com sucesso!");
     setIsOpen(false);
-    resetForm();
     resetSlide();
   }
 
   return (
-    <form onSubmit={handleCreateEntry} className="flex flex-col">
-      {/* date */}
+    <form onSubmit={handleSubmit(handleCreateEntry)} className="flex flex-col">
       <div className="flex flex-col">
         <input
           type="date"
           placeholder="Data"
-          value={
-            formData.createdAt
-              ? new Date(formData.createdAt).toISOString().split("T")[0]
-              : new Date().toISOString().split("T")[0]
-          }
-          onChange={(e) =>
-            setFormData({
-              ...formData,
-              createdAt: new Date(e.target.value),
-            })
-          }
+          {...register("createdAt", {
+            valueAsDate: true,
+          })}
         />
+        {errors.createdAt && <span>{errors.createdAt.message}</span>}
       </div>
       <div className="flex flex-col">
         <input
           type="text"
-          name="description"
-          id="description"
           placeholder="description"
-          value={formData.description}
-          onChange={(e) =>
-            setFormData({ ...formData, description: e.target.value })
-          }
+          {...register("description")}
         />
+        {errors.description && (
+          <span className="text-[10px]">{errors.description.message}</span>
+        )}
       </div>
       <div className="flex flex-col">
-        <input
-          type="text"
-          name="locale"
-          placeholder="Local"
-          value={formData.locale}
-          onChange={(e) => setFormData({ ...formData, locale: e.target.value })}
-        />
+        <input type="text" placeholder="Local" {...register("locale")} />
+        {errors.locale && <span>{errors.locale.message}</span>}
       </div>
       <div className="flex flex-col">
         <input
           type="number"
-          name="amount"
           placeholder="Valor"
-          value={formData.amount}
-          onChange={(e) =>
-            setFormData({
-              ...formData,
-              amount: Number(e.target.value),
-            })
-          }
+          {...register("amount", { valueAsNumber: true })}
         />
+        {errors.amount && <span>{errors.amount.message}</span>}
       </div>
       <div className="flex flex-col">
-        <button type="submit">Create</button>
+        <button
+          className="px-2 py-1 bg-zinc-700 rounded-lg shadow mt-4 text-zinc-100"
+          type="submit"
+        >
+          Create
+        </button>
       </div>
     </form>
   );
